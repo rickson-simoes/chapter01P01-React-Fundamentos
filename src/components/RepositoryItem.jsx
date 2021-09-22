073b00135c6dc46eee393c12f2e203f7b9f05108
Rickson <rickson.simoes@hotmail.com>
2021-09-21 23:42:11 -0300

    :hammer: Estado do componente

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,8 +1,8 @@
 export function RepositoryItem(props){
   return (
     <li>
-      <strong> { props.repository.name ?? "Default"} </strong>
-      <p>{ props.repository.description }</p>
+      <strong> {props.repository.name ?? "Default"} </strong>
+      <p>{props.repository.description }</p>
 
       <a href={props.repository.link}>
       Acessar repositório
