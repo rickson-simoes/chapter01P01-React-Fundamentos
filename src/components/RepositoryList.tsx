aa0b4c4f6d513a6b8e87f42066cab75492d8f348
Rickson <rickson.simoes@hotmail.com>
2021-09-23 17:09:05 -0300

    :hammer::wrench: Typescript / :package: Typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,19 +3,21 @@ import '../styles/repositories.scss';
 
 import { RepositoryItem } from "./RepositoryItem";
 
-//https://api.github.com/users/rickson-simoes/repos
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]) ;
 
   useEffect(() => {
     const data = fetch("https://api.github.com/users/rickson-simoes/repos")
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
-
-
-
 
   return (
     <section className="repository-list">
